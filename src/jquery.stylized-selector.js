4bd81a94665505fb242f8913c4f4de0a2526408c
Alejandro <aganglada@gmail.com>
2014-10-05 15:04:02 +0200

    Adding test

---
+++ src/jquery.stylized-selector.js
@@ -0,0 +1,332 @@
+/**
+ * jquery.stylized-selector.js
+ * -----------------------------
+ * @author aganglada
+ * @since 06/01/2014
+ * @link https://github.com/aganglada/jquery-stylized-selector
+ * @version 1.0
+ *
+ */
+
+(function ( $ )
+{
+    $.fn.stylizedSelector = function(options)
+    {
+        var selectCount = 0,
+
+            UP = 38, DOWN = 40, SPACE = 32, RETURN = 13, TAB = 9, ESC = 27, DELETE = 8,
+
+            timer = undefined,
+
+            defaults = {
+                hiddenClass: 	        'hidden',
+                wrapperClass: 			'select',
+                styledSelectorClass: 	'styled-select',
+                optionsClass: 			'options',
+                openSelectorClass: 		'open',
+                activeOptionClass: 		'selected',
+                currentTextClass: 		'current',
+                isMultipleClass: 		'multiple',
+                arrowIconClass: 		false,
+                onOpen: 				function(){},
+                onChange: 				function(){}
+            },
+
+            settings = $.extend(
+                {},
+                defaults,
+                options
+            ),
+
+            updateDataSelector = function($selector)
+            {
+                $selector.data( {
+                    'isMultiple':       $selector.attr('multiple') !== undefined,
+                    'styledSelect':     $selector.next('div.' + settings.styledSelectorClass),
+                    'optionList':       $selector.children(),
+                    'optionSelected':   $selector.children().filter(':selected')
+                });
+            },
+
+            dataSelector = function($selector, index)
+            {
+                return $selector.data(index);
+            },
+
+            buildSelector = function($selector)
+            {
+                var isMultipleClass = dataSelector($selector, 'isMultiple') ? ' ' + settings.isMultipleClass : '';
+
+                $selector.attr('tabindex', -1)
+                    .addClass(settings.hiddenClass)
+                    .wrap('<div data-id="' + selectCount + '" class="' + settings.wrapperClass + isMultipleClass + '"></div>');
+
+
+                if (!dataSelector($selector, 'isMultiple'))
+                {
+                    $selector.after('<div class="' + settings.styledSelectorClass + '">' +
+                        '<span class="' + settings.currentTextClass + '"></span>' +
+                        '</div>');
+
+                    setSelectedOption($selector);
+
+                    if (settings.arrowIconClass !== false)
+                    {
+                        var $styledSelect = $selector.next('div.' + settings.styledSelectorClass);
+                        $styledSelect.append('<i class="' + settings.arrowIconClass + '"></i>');
+                    }
+                }
+            },
+
+            setSelectedOption = function($selector)
+            {
+                updateDataSelector($selector);
+
+                var $selectedOption = dataSelector($selector, 'optionSelected'),
+                    $styledSelect   = dataSelector($selector, 'styledSelect'),
+                    $currentOption  = $styledSelect.children();
+
+                $currentOption.html($selectedOption.text());
+            },
+
+            updateSelect = function($selector)
+            {
+                updateDataSelector($selector);
+
+                var $selectedOption = dataSelector($selector, 'optionSelected'),
+                    $styledSelect   = dataSelector($selector, 'styledSelect');
+
+                var $list = $('<ul />', {
+                    'class': settings.optionsClass
+                }).insertAfter($styledSelect ? $styledSelect : $selector);
+
+                for (var i = 0; i < dataSelector($selector, 'optionList').length; i++)
+                {
+                    $('<li />', {
+                        text: $selector.children('option').eq(i).text(),
+                        rel: $selector.children('option').eq(i).val(),
+                        'class': $selectedOption[0] && $selectedOption[0].index == i ? settings.activeOptionClass : ''
+                    }).appendTo($list);
+                }
+            },
+
+            toggleOpen = function(event)
+            {
+                event.stopPropagation();
+
+                var $styledSelect = $(event.target).closest('.' + settings.styledSelectorClass),
+                    $selectorWrapper = $styledSelect.parent(),
+                    selectorIsOpen = $styledSelect.is('.' + settings.openSelectorClass);
+
+                closeAll();
+
+                if (!selectorIsOpen)
+                {
+                    $styledSelect.toggleClass(settings.openSelectorClass).next('ul.' + settings.optionsClass).toggle();
+                    $('select', $selectorWrapper).trigger('onOpen');
+                }
+            },
+
+            closeAll = function()
+            {
+                var $styledSelectors = $('.' + settings.styledSelectorClass),
+                    $selectorWrapper = $styledSelectors.parent();
+
+                $styledSelectors.removeClass(settings.openSelectorClass);
+                $('ul', $selectorWrapper).hide();
+            },
+
+            selectListItem = function(event)
+            {
+                selectItem(event, $(event.target).closest('li'));
+            },
+
+            selectItem = function(event, $_optionClicked)
+            {
+                event.stopPropagation();
+
+                var $selectorWrapper = $_optionClicked.closest('.' + settings.wrapperClass),
+                    $currentOption = $selectorWrapper.find('.' + settings.currentTextClass),
+                    $list = $selectorWrapper.find('ul'),
+                    $selector = $selectorWrapper.find('select');
+
+                $currentOption.text($_optionClicked.text());
+                $list.children('li').removeClass(settings.activeOptionClass);
+                $_optionClicked.addClass(settings.activeOptionClass);
+                $selector.val($_optionClicked.attr('rel'));
+
+                if (event.type == 'click')
+                {
+                    $selectorWrapper.find('.' + settings.styledSelectorClass).removeClass(settings.openSelectorClass);
+                    $list.hide();
+                    $selector.trigger('onChange', [{
+                        'selectedValue': $_optionClicked.attr('rel')
+                    }]);
+                }
+            },
+
+            onDefaultOpen = function(event)
+            {
+                var target = event.target,
+                    $selectorWrapper = $(target).parent(),
+                    $list = $selectorWrapper.find('ul'),
+                    $selectedOption = $list.children('li').filter('.' + settings.activeOptionClass);
+
+                reloadSelectedPosition($list, $selectedOption);
+                settings.onOpen.call(this);
+            },
+
+            reloadSelectedPosition = function($list, $current)
+            {
+                $list.scrollTop($list.scrollTop() + $current.position().top - $list.height()/2 + $current.height()/2);
+            },
+
+            selectKeyPress = function(event)
+            {
+                var $currentOpenSelector = $('.' + settings.styledSelectorClass + '.' + settings.openSelectorClass),
+                    $currentWrapperSelector = $currentOpenSelector.parent(),
+                    $selector = $('select', $currentWrapperSelector),
+                    $list = $('ul', $currentWrapperSelector),
+                    $current = $('li.' + settings.activeOptionClass, $list),
+                    $currentPrev = $current.prev(),
+                    $currentNext = $current.next(),
+                    matchString = '';
+
+                // keyboard conditions
+                var currentKeyCode = event.keyCode,
+                    keyboard = event.keyCode >= 48 && event.keyCode <= 90,
+                    numeric  = event.keyCode >= 96 && event.keyCode <= 105;
+
+                if ($currentOpenSelector.length > 0)
+                {
+                    event.preventDefault();
+                }
+
+                matchString = $selector.data('matchString') !== undefined
+                    ?
+                    $selector.data('matchString')
+                    :
+                    '';
+
+                if (
+                    currentKeyCode == RETURN
+                        ||
+                        currentKeyCode == SPACE
+                        ||
+                        currentKeyCode == TAB
+                        ||
+                        currentKeyCode == ESC
+                    )
+                {
+                    $current.trigger('click');
+                    return;
+                }
+                else if (currentKeyCode == UP && $currentPrev.length && $currentPrev.is('li'))
+                {
+                    selectItem(event, $currentPrev);
+                    reloadSelectedPosition($list, $current);
+                }
+                else if (currentKeyCode == DOWN && $currentNext.length && $currentNext.is('li'))
+                {
+                    selectItem(event, $currentNext);
+                    reloadSelectedPosition($list, $current);
+                }
+                else if (currentKeyCode == DELETE)
+                {
+                    matchString = matchString.slice(0, -1);
+                    $selector.data(
+                        'matchString',
+                        matchString
+                    );
+                }
+                else if (keyboard || numeric)
+                {
+                    currentKeyCode = numeric ? currentKeyCode -48 : currentKeyCode;
+                    matchString = matchString + String.fromCharCode(currentKeyCode).toLowerCase();
+                    $selector.data(
+                        'matchString',
+                        matchString
+                    );
+                    checkForMatch(event, $currentOpenSelector);
+                }
+
+                timer = $selector.data('timer');
+
+                if (timer !== undefined)
+                {
+                    clearTimeout(timer);
+                }
+
+                timer = setTimeout(
+                    callClearTimeout,
+                    3000
+                );
+
+                $selector.data('timer', timer);
+            },
+
+            callClearTimeout = function()
+            {
+                var $currentOpenSelector = $('.' + settings.styledSelectorClass + '.' + settings.openSelectorClass),
+                    $selector = $('select', $currentOpenSelector.parent());
+
+                clearKeyStrokes($selector);
+            },
+
+            checkForMatch = function(event, $currentOpenSelector)
+            {
+                var $openSelectorWrapper = $currentOpenSelector.parent(),
+                    $list = $('ul', $openSelectorWrapper),
+                    matchString = $('select', $openSelectorWrapper).data('matchString');
+
+                $('li', $list).each(function(key, element)
+                {
+                    var text = $(element).text().toLowerCase().trim();
+
+                    if (text.indexOf(matchString) >= 0)
+                    {
+                        var $selectedOption = $(this);
+                        selectItem(event, $selectedOption);
+                        reloadSelectedPosition($list, $selectedOption);
+                        return false;
+                    }
+                });
+            },
+
+            clearKeyStrokes = function($selector)
+            {
+                $selector.data('matchString', '');
+                timer = undefined;
+            };
+
+        /**
+         * jQuery plugin Loop
+         *
+         * Add a handler to manage the original select
+         *
+         */
+
+        this.each(function()
+        {
+            var $selector = $(this);
+
+            updateDataSelector($selector);
+            buildSelector($selector);
+            updateSelect($selector);
+            selectCount++;
+        });
+
+        $(document).on('click', '.' + settings.styledSelectorClass, toggleOpen);
+        $(document).on('click', closeAll);
+        $(document).on('click', '.' + settings.optionsClass, selectListItem);
+        $(document).on('keydown', selectKeyPress);
+        $(document).on('onOpen', 'select', onDefaultOpen);
+        $(document).on('onChange', 'select', function(event, data)
+        {
+            settings.onChange.call(this, data.selectedValue);
+        });
+
+        return this;
+    };
+
+}( jQuery ));
